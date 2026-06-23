f79f17d9fd06abea95e0e1c1d494154b0c3823c3
saket-nugget <saketramavedula@gmail.com>
2026-06-23 18:34:16 +0530

    chore: clean up repo by removing scratch files and pycache

--- beacon-backend/check-db.ts
+++
@@ -1,9 +0,0 @@
-import { PrismaClient } from '@prisma/client';
-const prisma = new PrismaClient();
-
-async function main() {
-  const signals = await prisma.signal.findMany();
-  console.log(JSON.stringify(signals, null, 2));
-}
-
-main().catch(console.error).finally(() => prisma.$disconnect());
